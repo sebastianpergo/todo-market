2d4e17973dc4ef2c9752f0a6bafaf8d793576593
sebastianpergo <sebastianperdomopg@gmail.com>
2023-06-10 20:45:55 -0500

    Initialize the project

---
+++ src/test/java/com/todomarket/TodoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
